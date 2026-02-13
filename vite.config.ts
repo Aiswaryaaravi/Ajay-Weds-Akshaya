86676127016ec2641218d99902ffffad2422102f
Aiswaryaaravi <r.aiswarya2004@gmail.com>
2026-02-13 23:31:08 +0530

    ver

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Ajay-Weds-Akshaya/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
